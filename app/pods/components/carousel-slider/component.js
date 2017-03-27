609c30d7e32f19abcc07e0d00f9a6368fb6ad463
Sam Selikoff <sam.selikoff@gmail.com>
2017-03-27 13:22:13 -0700

    Strip declarative example

--- app/pods/components/carousel-slider/component.js
+++ app/pods/components/carousel-slider/component.js
@@ -6,26 +6,14 @@ export default Ember.Component.extend({
 
   selectedIndex: 0,
 
-  currentSlide: Ember.computed('selectedIndex', function() {
-    let i = this.get('selectedIndex');
-
-    return this.get(`slides.${i}`);
+  currentSlide: Ember.computed(function() {
   }),
 
-  displayShift: Ember.computed('selectedIndex', function() {
-    let i = this.get('selectedIndex');
-    let width = 80; // from css
-    let margin = 3; // from css
-    let offset = 15.5;
-
-    let shifted = ((width + margin) * -i) + offset;
-
-    return `transform: translateX(${shifted}%)`;
+  displayShift: Ember.computed('', function() {
   }),
 
   actions: {
-    selectSlide(index) {
-      this.set('selectedIndex', index);
+    selectSlide() {
     }
   }
 
