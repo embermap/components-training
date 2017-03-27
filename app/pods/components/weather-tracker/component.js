1c9dabb6f391b81e16a44b2b8d9b09d82fbba112
Sam Selikoff <sam.selikoff@gmail.com>
2017-03-27 07:51:28 -0700

    Data-loader

--- app/pods/components/weather-tracker/component.js
+++ app/pods/components/weather-tracker/component.js
@@ -1,15 +1,5 @@
 import Ember from 'ember';
-import { task } from 'ember-concurrency';
 
 export default Ember.Component.extend({
-
-  loadWeather: task(function*(zip) {
-    if (zip.length < 5) {
-      return;
-    }
-
-    let weather = yield Ember.$.get('/weather');
-    this.set('weather', weather);
-  }).restartable()
 
 });
