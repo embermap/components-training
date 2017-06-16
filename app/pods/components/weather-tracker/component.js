94dd92ec12a43ada076ed27c231c56ad1c97d526
Amy Lam <alam@fastly.com>
2017-06-16 13:05:53 -0700

    Step 4 weather

--- app/pods/components/weather-tracker/component.js
+++ app/pods/components/weather-tracker/component.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isLoading: null,
+  weather: null,
+  zip: null,
+
+  actions: {
+    loadWeather(zip) {
+      if (zip.length === 5) {
+        this.set('isLoading', true);
+        Ember.$.get('/weather')
+        .then((response) => {
+          this.set('weather', response);
+          this.set('isLoading', false);
+        })
+      }
+    }
+  }
 
 });
