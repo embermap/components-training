82806bb9e640fe6a31ed2440f76a6cb6ae71cd7d
Amy Lam <alam@fastly.com>
2017-06-16 12:35:14 -0700

    Step 1: add c-panel functionality

--- app/pods/components/c-panel/component.js
+++ app/pods/components/c-panel/component.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isOpen: false,
+
   actions: {
     toggleIsOpen() {
-      console.log('sup');
+      this.toggleProperty('isOpen');
     }
   }
 });
